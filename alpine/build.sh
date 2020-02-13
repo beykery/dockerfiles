f70d9214ec676d84fe9f869a4d265264d33dabc9
yangchaoqun <yangchaoqun@huobi.com>
2020-02-13 12:03:42 +0800

    init .

---
+++ alpine/build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker build -t williamoony/alpine:3.11.3 .
+docker push williamoony/alpine:3.11.3
---
+++ java/8/build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker build -t williamoony/java8:202 .
+docker push williamoony/java8:202
