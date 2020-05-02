aef2f3939745ed6b09d4413a2e13d9d141ccbf9b
william <williamoony@gmail.com>
2020-05-03 00:34:04 +0800

    fix

--- elasticsearch/7.6.2/build.sh
+++ elasticsearch/7.6.2/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker build -t temp .
-docker tag temp williamoony/elasticsearch:7.6.2-smartcn
-docker push williamoony/elasticsearch:7.6.2-smartcn
+docker tag temp williamoony/elasticsearch:7.6.2-cn
+docker push williamoony/elasticsearch:7.6.2-cn
 docker rmi temp
 
