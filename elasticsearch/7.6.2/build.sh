ef3ff9f3aa2f486c2c6070493dd7adddac1395e8
yangchaoqun <yangchaoqun@huobi.com>
2020-05-13 19:20:13 +0800

    fix

--- elasticsearch/7.6.2/build.sh
+++ elasticsearch/7.6.2/build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+cd elasticsearch-analysis-ik-7.6.2
+zip -r elasticsearch-analysis-ik-7.6.2.zip *
+mv elasticsearch-analysis-ik-7.6.2.zip ../
+cd ..
 docker build -t temp .
 docker tag temp williamoony/elasticsearch:7.6.2-cn
 docker push williamoony/elasticsearch:7.6.2-cn
 docker rmi temp
-
+rm -f elasticsearch-analysis-ik-7.6.2.zip
