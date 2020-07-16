1fb7907cbceb9ff0cf58687f7b64ccaf132bdb39
beykery <874343737@qq.com>
2020-07-17 02:35:47 +0800

    fix

--- alpine/build.sh
+++ alpine/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t williamoony/alpine:3.11.3 .
+docker build --rm=true --no-cache --disable-content-trust=true -t williamoony/alpine:3.11.3 .
 docker push williamoony/alpine:3.11.3
