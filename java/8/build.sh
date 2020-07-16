e479fd8193905d9c65e06b72c69dd8142c557c05
beykery <874343737@qq.com>
2020-07-17 02:36:46 +0800

    fix

--- java/8/build.sh
+++ java/8/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t java8:202 .
+docker build --rm=true --no-cache --disable-content-trust=true  -t java8:202 .
 docker tag java8:202 williamoony/java8:202
 docker push williamoony/java8:202
