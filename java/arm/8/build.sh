5478280e2ea794c63fac902d4764fab8a238f5e5
yangchaoqun <yangchaoqun@huobi.com>
2020-08-19 11:19:29 +0800

    7.8.1

--- java/arm/8/build.sh
+++ java/arm/8/build.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t temp .
-docker tag temp williamoony/java8-arm:212
+docker build -t williamoony/java8-arm:212 .
 docker push williamoony/java8-arm:212
-docker rmi temp
